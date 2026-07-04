5d9db8cff86450535bef3675773526404ad8dce3
Admond <admond.tamang@oneaccord.cc>
2026-07-04 23:38:49 +0545

    feat: add server startup log message to console

--- exmaples/goserver/main.go
+++ exmaples/goserver/main.go
@@ -9,5 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello from k8s -v2")
 	})
+	fmt.Println("server is running")
 	http.ListenAndServe(":8080", nil)
 }
